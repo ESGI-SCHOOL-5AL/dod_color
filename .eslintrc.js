6e8d8a9529f045d74b486b147436fb0702622e3b
SystemGlitch <jeremy.la@outlook.fr>
2021-04-22 10:43:46 +0200

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/no-v-html": "off",
+    "prettier/prettier": ["error", { semi: false }],
+    "vue/html-self-closing": "off",
+    semi: [2, "never"],
   },
-};
+}
